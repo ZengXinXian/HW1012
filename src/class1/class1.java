c08eaa7ed0458a9845a8631366186098428fb6e3
ZengXinXian <t110360124@ntut.org.tw>
2023-10-11 16:58:06 +0800

    HW1012

---
+++ src/class1/class1.java
@@ -0,0 +1,25 @@
+package class1;
+
+/*import java.io.BufferedReader;
+import java.io.File;
+import java.io.*;*/
+
+public class class1 
+{
+	public static void main(String[] args) 
+	{
+		int d=2;
+		double pi=3.14;
+		
+		System.out.println("直徑是"+d+"公分的圓");
+		
+		System.out.println("其圓周圍"+(d*pi)+"公分");
+		
+		int num1=5;
+		int num2=4;
+		double div1=num1/num2;
+		double div2=(double)num1/(double)num2;
+		System.out.println("5//4等於"+div1);
+		System.out.println("5//4等於"+div2);
+	}
+}
